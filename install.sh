fb01243f9ffec5459d3b15fa40938d191f16b86e
Milos Mosovsky <milos.mosovsky@toptal.com>
2016-10-22 19:13:22 +0200

    Update install script and add gitignore

--- install.sh
+++ install.sh
@@ -1,32 +1,39 @@
-#!/bin/bash
-############################
-# .install.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-# http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
-############################
+#!/usr/bin/env bash
 
-########## Variables
+# Dotfiles
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig tmux.conf tmux.style"    # list of files/folders to symlink in homedir
+echo "Linking dotfiles..."
+overwrite_all=false
 
-##########
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+for file in $DIR/files/*; do
+  overwrite=false
 
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+  basename=${file##*/}
+  target="$HOME/.$basename"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+
+  if [ -e "$target" ] || [ -L "$target" ]; then
+    if ! $overwrite_all; then
+      while true; do
+        echo "$target already exists"
+        printf "[s]kip, [S]kip all, [o]verwrite, [O]verwrite all: "
+        read answer
+        case $answer in
+          "s") continue 2;;
+          "S") break 2;;
+          "o") overwrite=true; break;;
+          "O") overwrite_all=true; break;;
+           * ) continue;;
+        esac
+      done
+    fi
+  fi
+
+  if [ -e "$target" ] || [ -L "$target" ]; then
+    rm $target
+  fi
+
+  ln -svs "$file" "$target"
 done
